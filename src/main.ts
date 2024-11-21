07b0733bbb239a58564c998580df7b8e1dba8c09
Petter Lindh <petter.s.lindh@gmail.com>
2024-11-21 08:27:58 +0100

    Initial Commit.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { LandingComponent} from './app/components/landing/landing.component';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(LandingComponent, {
+  providers: [
+    provideRouter(routes) // Lägg till routes här om det behövs
+  ],
+}).catch((err) => console.error(err));
