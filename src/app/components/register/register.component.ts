924d9db1b2aa1e317beef8edd9e240ebc8efc503
Petter Lindh <petter.lindh@skoglit.se>
2024-12-02 16:48:35 +0100

    Begin adding registration but with errors.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,57 @@
-import { Component } from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [
-    ReactiveFormsModule
-  ],
+  imports: [ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent implements OnInit {
+  registerForm = FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+  })
+
+  }
+}
+
+/*
+
+import {Component} from '@angular/core';
+import {AbstractControl, FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm;
 
+  constructor(private fb: FormBuilder) {
+
+    this.registerForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],   // Validators.email = Email Structure
+      password: ['', [Validators.required, Validators.minLength(6), Validators.pattern(/(?=.*[ˆa-zA-Z0-9 ])/)]],
+      confirmPassword: [''],
+    }, {Validators:this.passwordMatchValidator})
+
+    passwordMatchValidator: ValidatorFn = (control: AbstractControl): null => {
+    }
+
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value)
+  }
 }
+*/
