bcaa7f3e80aedb3196c9f5692b622adc2ce0e80f
Petter Lindh <petter.s.lindh@gmail.com>
2024-12-10 09:11:48 +0100

    No logout icon when not logged in.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { UserService } from './services/user.service';
 import { AuthService } from './services/auth.service';
 import { claimReq } from './utils/claimReq-utils';
 import { HideIfClaimsNotMetDirective } from './directives/hide-if-claims-not-met.directive';
+import {NgIf} from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HideIfClaimsNotMetDirective],
+  imports: [RouterOutlet, HideIfClaimsNotMetDirective, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -18,10 +19,13 @@ export class AppComponent  implements OnInit {
 
   constructor(private router: Router, private userService: UserService, private authService: AuthService) {}
 
+  isLoggedIn = false;
   fullName: string = '';
 
+
   ngOnInit() {
-    this.userService.getUserProfile().subscribe({
+      this.isLoggedIn = this.authService.isLoggedIn();
+      this.userService.getUserProfile().subscribe({
       next: (res: any) => this.fullName = res.fullName,
       error: (err: any) => console.log('error while retrieving user profile:\n', err)
     })
--- src/app/utils/claimReq-utils.ts
+++ src/app/utils/claimReq-utils.ts
@@ -2,6 +2,6 @@
   adminOnly: (c: any) => c.role == "Admin",
   adminOrOwner: (c: any) => c.role == "Admin" || c.role == "Owner",
   authenticated: (c: any) => c.isAuthenticated,
-  notAuthenticated: (c: any) => !c.isAuthenticated
+  notAuthenticated: (c: any) => !c.isAuthenticated,
 };
 
